93a2b3498e39c5aa691ae8472dc0e7c0d29b9711
devplayg <devplayg@gmail.com>
2018-03-11 16:12:34 +0900

    changed routers

--- routers/router.go
+++ routers/router.go
@@ -6,23 +6,22 @@ import (
 )
 
 func init() {
-	// 로그인
-    beego.Router("/", &controllers.LoginController{}, "get:Get")
-	beego.Router(`/signin`, &controllers.LoginController{})
-	beego.Router(`/signout`, &controllers.LoginController{}, "*:Logout")
-	beego.Router(`/signin/:username([\w]+)/salt`, &controllers.LoginController{}, "Get:GetPasswordSalt")
-    ///login/member/salt
 
-    // 대시보드
+	// 대시보드
 
-    // IPAS 로그
+	// IPAS 로그
 	beego.Router(`/ipaslogs`, &controllers.IpaslogController{})
 
 	// 사용자
 	beego.Router(`/members/`, &controllers.MemberController{})
-	//beego.Router(`/members/, &controllers.IpaslogController{}, "get:GetLogs")
 
-    // 제어어
+	// 제어
 
-    // 환경설정
+	// 환경설정
+
+	// 로그인
+	beego.Router("/", &controllers.LoginController{}, "get:Get")
+	beego.Router(`/signin`, &controllers.LoginController{})
+	beego.Router(`/signout`, &controllers.LoginController{}, "*:Logout")
+	beego.Router(`/signin/:username([\w]+)/salt`, &controllers.LoginController{}, "Get:GetPasswordSalt")
 }
