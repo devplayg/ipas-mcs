07b6258f6d5b0f9716d5deeaec493d95023de10f
devplayg <devplayg@gmail.com>
2018-03-13 23:17:32 +0900

    test input regex test regex patterns (RE2)

--- objs/member.go
+++ objs/member.go
@@ -1,7 +1,15 @@
 package objs
 
 import (
+	"errors"
+	"github.com/devplayg/ipas-mcs/libs"
+	"unicode/utf8"
+	"github.com/go-ozzo/ozzo-validation"
+	log "github.com/sirupsen/logrus"
+	"regexp"
+	"strings"
 	"time"
+	"unicode"
 )
 
 type Member struct {
@@ -26,6 +34,67 @@ type Member struct {
 	//Status             int `json:"-"`
 	//AllowedIp          string
 	//Usergroups         []int `json:"-"`
+}
+
+func (a Member) Validate() error {
+
+	if !isValidPassword(a.Password) {
+		return errors.New("invalid password")
+	}
+	log.Debugf("Name count: %d ", utf8.RuneCountInString(a.Name))
+
+	return validation.ValidateStruct(&a,
+		validation.Field(
+			&a.Username,
+			validation.Required,
+			validation.Match(regexp.MustCompile("^[[:alpha:]]{1}[[:word:]]{3,16}$")),
+		),
+		validation.Field(
+			&a.Password,
+			validation.Required,
+			validation.Length(9, 16),
+		),
+		validation.Field(
+			&a.Name,
+			validation.Required,
+			validation.RuneLength(2, 16),
+		),
+		validation.Field(
+			&a.Email,
+			validation.Required,
+			validation.Match(regexp.MustCompile(libs.EmailPattern)),
+			validation.Length(8, 254),
+		),
+	)
+}
+
+func isValidPassword(str string) bool {
+	hasLowerCase := false
+	hasUpperCase := true
+	hasNumber := false
+	hasSpecialChar := false
+
+	specialChars := "!@#$%^&*()~_+`-=,.<>/?|"
+	for _, s := range str {
+		if unicode.IsLower(s) {
+
+			hasLowerCase = true
+			log.Debugf("%#U lower", s)
+		} else if unicode.IsUpper(s) {
+			hasUpperCase = true
+			log.Debugf("%#U upper", s)
+		} else if unicode.IsNumber(s) {
+			hasNumber = true
+			log.Debugf("%#U number", s)
+		} else if strings.ContainsRune(specialChars, s) {
+			hasSpecialChar = true
+			log.Debugf("%#U symbol", s)
+		} else {
+			log.Debugf("%#U nothing", s)
+		}
+	}
+
+	return hasLowerCase && hasUpperCase && hasNumber && hasSpecialChar
 }
 
 //
