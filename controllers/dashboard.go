40ee1b8d511f0a05520796e9e707a804a909ebf4
devplayg <devplayg@gmail.com>
2018-05-01 10:33:55 +0900

    added dashboard

--- controllers/dashboard.go
+++ controllers/dashboard.go
@@ -1,1 +1,23 @@
 package controllers
+
+import "github.com/devplayg/ipas-mcs/objs"
+
+type DashboardController struct {
+	baseController
+}
+
+func (c *DashboardController) CtrlPrepare() {
+	// 추가 언어 키워드
+	c.addToFrontLang("ipas.start,shock,speeding,proximity")
+
+	// 권한 부여
+	c.grant(objs.User)
+}
+
+func (c *DashboardController) diplay() {
+	// 추가 언어 키워드
+	c.addToFrontLang("ipas.start,shock,speeding,proximity")
+
+	// 권한 부여
+	c.setTpl("dashboard.tpl")
+}
